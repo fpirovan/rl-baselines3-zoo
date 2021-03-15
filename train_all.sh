2191bf7ac4e07d3d2e05150fb70afbb387969c34
Federico Pirovano <fpirovan@student.ethz.ch>
2021-03-15 20:05:36 +0100

    Minor

---
+++ train_all.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+proj_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+environments_dir="${proj_dir}/environments/assets"
+
+export PYTHONPATH=$PYTHONPATH:$proj_dir
+cd "$proj_dir"
+
+env_ids="Ant-v2 HalfCheetah-v2 Hopper-v2 Humanoid-v2 Walker2d-v2"
+for env_xml in "$environments_dir"/*.xml; do
+  env_id=$(basename -- "$env_xml")
+  env_id="${env_id%.*}"
+  env_id="${env_id}-v1"
+  env_ids="${env_ids} ${env_id}"
+done
+
+for env_id in $env_ids; do
+  python train.py --seed 1085 --env_id "$env_id"
+done
