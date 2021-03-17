690994ca5c62fc8ac2f3d190f6da5f8592c0819b
Federico Pirovano <fpirovan@student.ethz.ch>
2021-03-17 13:00:25 +0100

    Minor

--- environments/__init__.py
+++ environments/__init__.py
@@ -1,1 +1,13 @@
+from stable_baselines3 import A2C, DDPG, DQN, HER, PPO, SAC, TD3
+
 from environments.register import register_all_envs
+
+ALGOS = {
+    "a2c": A2C,
+    "ddpg": DDPG,
+    "dqn": DQN,
+    "ppo": PPO,
+    "her": HER,
+    "sac": SAC,
+    "td3": TD3
+}
