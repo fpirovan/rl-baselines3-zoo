d31b6c5d8b39d5b5e7a8773903d98b18f6d73551
Federico Pirovano <fpirovan@student.ethz.ch>
2021-03-18 15:22:32 +0100

    Minor

--- train_all.sh
+++ train_all.sh
@@ -3,7 +3,7 @@
 proj_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 environments_dir="${proj_dir}/environments/assets"
 
-env_ids=$(python gen_str_train_all.py --env_ids Ant-v2 HalfCheetah-v2 Hopper-v2 Humanoid-v2 Walker2d-v2 new)
+env_ids=$(python gen_str_train_all.py --env_ids "$@")
 
 export PYTHONPATH="${PYTHONPATH}:${proj_dir}"
 cd "$proj_dir"
