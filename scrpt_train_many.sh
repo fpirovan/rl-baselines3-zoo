150f1b247492005a1e53a28021d91230b446b5e6
Federico Pirovano <fpirovan@student.ethz.ch>
2021-03-25 11:28:04 +0100

    Minor

--- scrpt_train_many.sh
+++ scrpt_train_many.sh
@@ -3,7 +3,7 @@
 proj_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 export PYTHONPATH="${PYTHONPATH}:${proj_dir}"
-cmd=$(python train_many_str.py --seed 1085 "$@")
+cmd=$(python args_train_many.py --seed 1085 "$@")
 
 cd "$proj_dir"
 eval "$cmd"
